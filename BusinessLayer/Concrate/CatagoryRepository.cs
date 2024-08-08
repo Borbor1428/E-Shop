9088795bc80dfb59d927a19eadf48e662614d34c
Bora Koç <132473954+Borbor1428@users.noreply.github.com>
2024-08-08 16:43:00 +0300

    Admin Paneli Kategori CRUD İşlemleri

---
+++ BusinessLayer/Concrate/CatagoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using EmptyLayer.Entities;
+
+namespace BusinessLayer.Concrate
+{
+    public class CatagoryRepository:GenericRepository<Category>
+    {
+    }
+}
---
+++ E-Shop/Controllers/AdminCategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Concrate;
+
+namespace E_Shop.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        CatagoryRepository categoryRepository = new CatagoryRepository();
+        public ActionResult Index()
+        {
+            return View(categoryRepository.List());
+        }
+    }
+}
